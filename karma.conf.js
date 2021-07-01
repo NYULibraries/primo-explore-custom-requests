6aa65a52dcb41ecced93f91e251717c5bcb7921b
BAlter <barnaby.alter@nyu.edu>
2021-07-01 00:09:57 -0400

    Don't run tests in a random order

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,7 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
+    client: { jasmine: { random: false } },
     frameworks: ['jasmine'],
     reporters: [
       'spec',
